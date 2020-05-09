b2f6c06d9b2c0f44c99b251bf1ab3221884dd155
Lee <letomok@gmail.com>
2020-05-09 16:19:57 +0900

    material-ui color palette add & change color

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { applyMiddleware, createStore } from "redux";
@@ -11,6 +13,17 @@ const devTools =
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__();
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            // light: will be calculated from palette.primary.main,
+            main: "#1976d2",
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+        },
+    },
+});
+
 const createStoreWithMiddleware = applyMiddleware(
     promiseMiddleware,
     ReduxThunk
@@ -18,9 +31,11 @@ const createStoreWithMiddleware = applyMiddleware(
 
 const Root = () => (
     <BrowserRouter>
-        <Provider store={createStoreWithMiddleware(Reducer, devTools)}>
-            <App />
-        </Provider>
+        <ThemeProvider theme={theme}>
+            <Provider store={createStoreWithMiddleware(Reducer, devTools)}>
+                <App />
+            </Provider>
+        </ThemeProvider>
     </BrowserRouter>
 );
 
